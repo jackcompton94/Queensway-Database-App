08fb8668ff47ced02509cbced46ee015f0b5aa15
jackcompton94 <jackcompton94@gmail.com>
2022-12-21 09:14:44 -0600

    update menu

--- queenswayDatabaseApp/LoginForm.h
+++ queenswayDatabaseApp/LoginForm.h
@@ -37,16 +37,11 @@ namespace queenswayDatabaseApp {
 			}
 		}
 
-
-
-
-
-
-	private: System::Windows::Forms::Panel^ panel2;
 	private: System::Windows::Forms::PictureBox^ pictureBox1;
-	private: System::Windows::Forms::Button^ registerButton;
 	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ registerButton;
 	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Panel^ panel2;
 	private: System::Windows::Forms::Label^ passwordLabel;
 	private: System::Windows::Forms::TextBox^ usernameTextBox;
 	private: System::Windows::Forms::Label^ usernameLabel;
@@ -54,11 +49,6 @@ namespace queenswayDatabaseApp {
 	private: System::Windows::Forms::Button^ signInButton;
 	private: System::Windows::Forms::Label^ label3;
 	private: System::Windows::Forms::Panel^ panel1;
-
-
-
-
-
 
 	protected:
 
@@ -79,11 +69,11 @@ namespace queenswayDatabaseApp {
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(LoginForm::typeid));
-			this->panel2 = (gcnew System::Windows::Forms::Panel());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->registerButton = (gcnew System::Windows::Forms::Button());
-			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->registerButton = (gcnew System::Windows::Forms::Button());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
 			this->passwordLabel = (gcnew System::Windows::Forms::Label());
 			this->usernameTextBox = (gcnew System::Windows::Forms::TextBox());
 			this->usernameLabel = (gcnew System::Windows::Forms::Label());
@@ -91,36 +81,33 @@ namespace queenswayDatabaseApp {
 			this->signInButton = (gcnew System::Windows::Forms::Button());
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
-			this->panel2->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->panel2->SuspendLayout();
 			this->panel1->SuspendLayout();
 			this->SuspendLayout();
 			// 
-			// panel2
+			// pictureBox1
 			// 
-			this->panel2->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"panel2.BackgroundImage")));
-			this->panel2->Controls->Add(this->label2);
-			this->panel2->Controls->Add(this->registerButton);
-			this->panel2->Controls->Add(this->label1);
-			this->panel2->Controls->Add(this->pictureBox1);
-			this->panel2->Dock = System::Windows::Forms::DockStyle::Left;
-			this->panel2->Location = System::Drawing::Point(0, 0);
-			this->panel2->Name = L"panel2";
-			this->panel2->Size = System::Drawing::Size(378, 388);
-			this->panel2->TabIndex = 6;
+			this->pictureBox1->BackColor = System::Drawing::Color::Transparent;
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(57, 12);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(250, 90);
+			this->pictureBox1->TabIndex = 0;
+			this->pictureBox1->TabStop = false;
 			// 
-			// label2
+			// label1
 			// 
-			this->label2->AutoSize = true;
-			this->label2->BackColor = System::Drawing::Color::Transparent;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Arial", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::Color::Transparent;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Arial", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label2->ForeColor = System::Drawing::SystemColors::Menu;
-			this->label2->Location = System::Drawing::Point(69, 148);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(223, 34);
-			this->label2->TabIndex = 6;
-			this->label2->Text = L"Database of Queensway shows \r\npulled from IG @queenswayband";
+			this->label1->ForeColor = System::Drawing::SystemColors::Menu;
+			this->label1->Location = System::Drawing::Point(141, 263);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(81, 16);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"No Account\?";
 			// 
 			// registerButton
 			// 
@@ -139,28 +126,31 @@ namespace queenswayDatabaseApp {
 			this->registerButton->UseVisualStyleBackColor = true;
 			this->registerButton->Click += gcnew System::EventHandler(this, &LoginForm::registerClicked);
 			// 
-			// label1
+			// label2
 			// 
-			this->label1->AutoSize = true;
-			this->label1->BackColor = System::Drawing::Color::Transparent;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Arial", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->label2->AutoSize = true;
+			this->label2->BackColor = System::Drawing::Color::Transparent;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Arial", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->ForeColor = System::Drawing::SystemColors::Menu;
-			this->label1->Location = System::Drawing::Point(137, 262);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(90, 17);
-			this->label1->TabIndex = 1;
-			this->label1->Text = L"No Account\?";
+			this->label2->ForeColor = System::Drawing::SystemColors::Menu;
+			this->label2->Location = System::Drawing::Point(69, 148);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(223, 34);
+			this->label2->TabIndex = 6;
+			this->label2->Text = L"Database of Queensway shows \r\npulled from IG @queenswayband";
 			// 
-			// pictureBox1
+			// panel2
 			// 
-			this->pictureBox1->BackColor = System::Drawing::Color::Transparent;
-			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(57, 12);
-			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(250, 90);
-			this->pictureBox1->TabIndex = 0;
-			this->pictureBox1->TabStop = false;
+			this->panel2->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"panel2.BackgroundImage")));
+			this->panel2->Controls->Add(this->label2);
+			this->panel2->Controls->Add(this->registerButton);
+			this->panel2->Controls->Add(this->label1);
+			this->panel2->Controls->Add(this->pictureBox1);
+			this->panel2->Dock = System::Windows::Forms::DockStyle::Left;
+			this->panel2->Location = System::Drawing::Point(0, 0);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(368, 353);
+			this->panel2->TabIndex = 6;
 			// 
 			// passwordLabel
 			// 
@@ -169,7 +159,7 @@ namespace queenswayDatabaseApp {
 			this->passwordLabel->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->passwordLabel->ForeColor = System::Drawing::SystemColors::Menu;
-			this->passwordLabel->Location = System::Drawing::Point(69, 134);
+			this->passwordLabel->Location = System::Drawing::Point(79, 134);
 			this->passwordLabel->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->passwordLabel->Name = L"passwordLabel";
 			this->passwordLabel->Size = System::Drawing::Size(69, 20);
@@ -186,10 +176,10 @@ namespace queenswayDatabaseApp {
 			this->usernameTextBox->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 11.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->usernameTextBox->ForeColor = System::Drawing::SystemColors::Menu;
-			this->usernameTextBox->Location = System::Drawing::Point(69, 104);
+			this->usernameTextBox->Location = System::Drawing::Point(79, 104);
 			this->usernameTextBox->Margin = System::Windows::Forms::Padding(2, 3, 2, 3);
 			this->usernameTextBox->Name = L"usernameTextBox";
-			this->usernameTextBox->Size = System::Drawing::Size(265, 25);
+			this->usernameTextBox->Size = System::Drawing::Size(181, 25);
 			this->usernameTextBox->TabIndex = 2;
 			// 
 			// usernameLabel
@@ -199,7 +189,7 @@ namespace queenswayDatabaseApp {
 			this->usernameLabel->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->usernameLabel->ForeColor = System::Drawing::SystemColors::Menu;
-			this->usernameLabel->Location = System::Drawing::Point(67, 82);
+			this->usernameLabel->Location = System::Drawing::Point(77, 82);
 			this->usernameLabel->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->usernameLabel->Name = L"usernameLabel";
 			this->usernameLabel->Size = System::Drawing::Size(71, 20);
@@ -216,10 +206,10 @@ namespace queenswayDatabaseApp {
 			this->passwordTextBox->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 11.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->passwordTextBox->ForeColor = System::Drawing::SystemColors::Menu;
-			this->passwordTextBox->Location = System::Drawing::Point(69, 157);
+			this->passwordTextBox->Location = System::Drawing::Point(79, 157);
 			this->passwordTextBox->Margin = System::Windows::Forms::Padding(2, 3, 2, 3);
 			this->passwordTextBox->Name = L"passwordTextBox";
-			this->passwordTextBox->Size = System::Drawing::Size(265, 25);
+			this->passwordTextBox->Size = System::Drawing::Size(181, 25);
 			this->passwordTextBox->TabIndex = 3;
 			this->passwordTextBox->Text = L"Password";
 			this->passwordTextBox->UseSystemPasswordChar = true;
@@ -233,10 +223,10 @@ namespace queenswayDatabaseApp {
 			this->signInButton->FlatAppearance->BorderSize = 0;
 			this->signInButton->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
 			this->signInButton->ForeColor = System::Drawing::SystemColors::Menu;
-			this->signInButton->Location = System::Drawing::Point(69, 282);
+			this->signInButton->Location = System::Drawing::Point(79, 282);
 			this->signInButton->Margin = System::Windows::Forms::Padding(2, 3, 2, 3);
 			this->signInButton->Name = L"signInButton";
-			this->signInButton->Size = System::Drawing::Size(265, 29);
+			this->signInButton->Size = System::Drawing::Size(181, 29);
 			this->signInButton->TabIndex = 4;
 			this->signInButton->Text = L"Sign in";
 			this->signInButton->UseVisualStyleBackColor = true;
@@ -249,7 +239,7 @@ namespace queenswayDatabaseApp {
 			this->label3->Font = (gcnew System::Drawing::Font(L"Arial", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->label3->ForeColor = System::Drawing::SystemColors::Menu;
-			this->label3->Location = System::Drawing::Point(19, 12);
+			this->label3->Location = System::Drawing::Point(26, 28);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(107, 32);
 			this->label3->TabIndex = 7;
@@ -265,11 +255,10 @@ namespace queenswayDatabaseApp {
 			this->panel1->Controls->Add(this->usernameLabel);
 			this->panel1->Controls->Add(this->usernameTextBox);
 			this->panel1->Controls->Add(this->passwordLabel);
-			this->panel1->Dock = System::Windows::Forms::DockStyle::Right;
-			this->panel1->Location = System::Drawing::Point(373, 0);
+			this->panel1->Location = System::Drawing::Point(348, 0);
 			this->panel1->Margin = System::Windows::Forms::Padding(2, 3, 2, 3);
 			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(405, 388);
+			this->panel1->Size = System::Drawing::Size(321, 388);
 			this->panel1->TabIndex = 5;
 			this->panel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &LoginForm::panel1_Paint);
 			// 
@@ -278,7 +267,7 @@ namespace queenswayDatabaseApp {
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
-			this->ClientSize = System::Drawing::Size(778, 388);
+			this->ClientSize = System::Drawing::Size(668, 353);
 			this->Controls->Add(this->panel2);
 			this->Controls->Add(this->panel1);
 			this->DoubleBuffered = true;
@@ -291,9 +280,9 @@ namespace queenswayDatabaseApp {
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"Sign in to the Queensway Database";
 			this->Load += gcnew System::EventHandler(this, &LoginForm::LoginForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->panel2->ResumeLayout(false);
 			this->panel2->PerformLayout();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->panel1->ResumeLayout(false);
 			this->panel1->PerformLayout();
 			this->ResumeLayout(false);
@@ -303,6 +292,9 @@ namespace queenswayDatabaseApp {
 
 	public:
 		User^ user = nullptr;
+
+	private: System::Void panel1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
+	}
 
 	private: 
 		System::Void registerClicked(System::Object^ sender, System::EventArgs^ e) {
@@ -355,7 +347,6 @@ namespace queenswayDatabaseApp {
 
 	private: System::Void LoginForm_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
-private: System::Void panel1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
-}
+	
 };
 }
